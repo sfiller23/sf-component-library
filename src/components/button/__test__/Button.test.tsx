cfefaf75a0c50ceac23e293a659e3bdff0633397
sfiller23 <sf27lp@gmail.com>
2024-04-24 18:19:29 +0300

    publish setup

---
+++ src/components/button/__test__/Button.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { describe, expect, it } from "vitest";
+import Button from "../Button";
+
+describe("Button component", () => {
+  it("Button should render correctly", () => {
+    render(<Button />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+});
