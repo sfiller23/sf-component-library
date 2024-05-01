84c371570b27d843c4078e8b01bf145c9a82e2e0
sfiller23 <sf27lp@gmail.com>
2024-05-01 17:42:59 +0300

    adding spaceSchema to theme.ts

---
+++ src/utils/theme.ts
@@ -0,0 +1,13 @@
+type SpaceSchemaType = {
+  [key: string]: `${number}rem`;
+};
+
+export const spaceSchema: SpaceSchemaType = {
+  xs: "0.125rem",
+  s: "0.25rem",
+  m: "0.5rem",
+  l: "1rem",
+  xl: "2rem",
+  xxl: "4rem",
+  none: "0rem",
+};
