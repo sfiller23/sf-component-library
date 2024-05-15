39964ee2e332c348fba663e6d2d3e7664ab17e2d
sfiller23 <sf27lp@gmail.com>
2024-05-15 06:20:50 +0300

    Columns update

--- src/UI/columns/Columns.tsx
+++ src/UI/columns/Columns.tsx
@@ -1,16 +1,18 @@
+import { PropsWithChildren } from "react";
 import { styled } from "styled-components";
 import { spaceSchema } from "../../utils/theme";
 
 interface ColumnsProps {
-  columns?: number;
   gutter?: keyof typeof spaceSchema;
+  evenly?: boolean;
 }
 
-const Columns = styled.div<ColumnsProps>`
-  --columns: 2;
+const Columns = styled.div<PropsWithChildren<ColumnsProps>>`
+  --columns: ${({ children }) => Array(children).length};
+  --pattern: ${({ evenly = true }) => (evenly ? "1fr" : "auto")};
   display: grid;
   gap: ${({ gutter = spaceSchema.xs }) => spaceSchema[gutter]};
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(var(--columns), var(--pattern));
 `;
 
 export default Columns;
