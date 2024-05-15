eaf8be428765e72a16739861326cf5a0d6729d48
sfiller23 <sf27lp@gmail.com>
2024-05-15 11:39:14 +0300

    adding styles to Typography

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -3,9 +3,18 @@ import { styled } from "styled-components";
 export interface TypographyProps {
   children: string;
   as?: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "h7";
+  color?: string;
+  size?: `${number}rem`;
+  weight?: number;
+  spacing?: string;
 }
 
-const StyledTypography = styled.p<TypographyProps>``;
+const StyledTypography = styled.p<TypographyProps>`
+  color: ${({ color = "black" }) => color};
+  font-size: ${({ size = "1rem" }) => size};
+  font-weight: ${({ weight = 400 }) => weight};
+  letter-spacing: ${({ spacing = "0" }) => spacing};
+`;
 
 const Typography = (props: TypographyProps) => {
   const { children, as = "p" } = props;
