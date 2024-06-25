651948356ab5de794e336775fa6c46335dd5cb5b
sfiller23 <sf27lp@gmail.com>
2024-06-25 13:08:13 +0300

    plaster fic in pad stories

--- src/UI/pad/__docs__/Pad.stories.tsx
+++ src/UI/pad/__docs__/Pad.stories.tsx
@@ -1,20 +1,26 @@
 import type { Meta, StoryFn } from "@storybook/react";
 import React from "react";
 import { styled } from "styled-components";
+import { PaddingMarginProps } from "../../../utils/common";
 import Pad from "../Pad";
 
 export default {
   title: "Pad",
   component: Pad,
+  argTypes: {
+    space: {
+      control: "text",
+    },
+  },
 } as Meta;
 
 const StyledDiv = styled.div`
   border: 1px solid black;
 `;
 
-const Template: StoryFn<{ span?: number }> = (args) => (
-  <StyledDiv>
-    <Pad {...args}>
+const Template: StoryFn<{ space: PaddingMarginProps }> = (args) => (
+  <StyledDiv {...args}>
+    <Pad space="l">
       <StyledDiv>div</StyledDiv>
     </Pad>
   </StyledDiv>
